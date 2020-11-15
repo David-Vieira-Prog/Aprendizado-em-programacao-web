0e6a391cec10dd2e7638d550136dfe5adf5c2754
David Vieira <davidvieira0110@gmail.com>
2020-11-15 16:19:20 -0300

    Primeira Pasta adicionada

---
+++ www/Repositorio_Sites/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href='assets/css/style.css'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índices dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h1>Módulo 1</h1>
+                    <ul>
+                        <li><a href="exercicio.php">Exercício A</a></li>
+
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">Desenvolvido por David Vieira | <?= date('Y'); ?> </footer>
+
+</body>
+
+</html>
